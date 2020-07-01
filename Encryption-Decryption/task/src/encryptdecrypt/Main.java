07c702083c2f90bf816424c6de68ec0bcaf280f9
apzivile <zivile.apo@gmail.com>
2020-07-01 16:11:50 +0100

    Encryption-Decryption project; finished 5th part

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,30 +1,65 @@
 package encryptdecrypt;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+       /* Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        String[] args = line.split(" ");
+        scanner.close();
+*/
         String operation = "enc";
         String text = "";
         int key = 0;
+        File fileIn = null;
+        File fileOut = null;
+        FileWriter writer = null;
         for (int i = 0; i < args.length; i++) {
             if ("-mode".equals(args[i])) {
                 operation = args[i + 1];
+                continue;
             }
             if ("-key".equals(args[i])) {
                 key = Integer.parseInt(args[i + 1]);
+                continue;
             }
             if ("-data".equals(args[i])) {
                 text = args[i + 1];
+                continue;
+            }
+            if ("-in".equals(args[i])) {
+                fileIn = new File(args[i + 1]);
+                continue;
+            }
+            if ("-out".equals(args[i])) {
+                fileOut = new File(args[i + 1]);
             }
         }
+        if (fileOut != null) {
+            writer = new FileWriter(fileOut);
+        }
+        if (fileIn != null && writer != null) {
+            Scanner sc = new Scanner(fileIn);
+            while (sc.hasNextLine()) {
+                text = sc.nextLine();
+            }
+            sc.close();
+        }
+
         if ("enc".equals(operation)) {
-            encryption(text, key);
+            encryption(text, key, writer);
         }
         if ("dec".equals(operation)) {
-            decryption(text, key);
+            decryption(text, key, writer);
         }
     }
 
-    public static void encryption(String text, int key) {
+    public static void encryption(String text, int key, FileWriter writer) throws IOException {
         String referenceForCharacters = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOQPRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
         String encryptedReferenceForChars = referenceForCharacters.substring(key) + referenceForCharacters.substring(0, key);
         String character;
@@ -38,10 +73,16 @@ public class Main {
                 }
             }
         }
-        System.out.println(newText);
+        try (writer) {
+            if (writer != null) {
+                writer.write(String.valueOf(newText));
+            } else {
+                System.out.println(newText);
+            }
+        }
     }
 
-    public static void decryption(String text, int key) {
+    public static void decryption(String text, int key, FileWriter writer) throws IOException {
         String referenceForCharacters = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOQPRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
         String character;
         StringBuilder newText = new StringBuilder();
@@ -58,7 +99,13 @@ public class Main {
                 }
             }
         }
-        System.out.println(newText);
+        try (writer) {
+            if (writer != null) {
+                writer.write(String.valueOf(newText));
+            } else {
+                System.out.println(newText);
+            }
+        }
     }
 }
 
