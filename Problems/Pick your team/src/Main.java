65f388aebe61258bf56bb1098ef0676ee0b812c7
apzivile <zivile.apo@gmail.com>
2020-07-04 16:11:33 +0100

    Encryption-Decryption project; strategy problems

---
+++ Problems/Pick your team/src/Main.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+
+class SelectionContext {
+
+    private PersonSelectionAlgorithm algorithm;
+
+    public void setAlgorithm(PersonSelectionAlgorithm algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    public Person[] selectPersons(Person[] persons) {
+        return algorithm.select(persons);
+    }
+}
+
+interface PersonSelectionAlgorithm {
+
+    Person[] select(Person[] persons);
+}
+
+class TakePersonsWithStepAlgorithm implements PersonSelectionAlgorithm {
+
+
+    public Person[] people;
+
+    public TakePersonsWithStepAlgorithm(int step) {
+        for (int i =0; i < people.length;i++ ){
+            if (i%step==0){
+                System.out.println("hey");
+            }
+        }
+    }
+
+    @Override
+    public Person[] select(Person[] persons) {
+        this.people = persons;
+        return persons;
+    }
+
+}
+
+
+class TakeLastPersonsAlgorithm implements PersonSelectionAlgorithm {
+
+    public TakeLastPersonsAlgorithm(int count) {
+
+    }
+
+    @Override
+    public Person[] select(Person[] persons) {
+        //for (int i =)
+        return persons;
+    }
+}
+
+class Person {
+
+    String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+}
+
+/* Do not change code below */
+public class Main {
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+
+        final int count = Integer.parseInt(scanner.nextLine());
+        final Person[] persons = new Person[count];
+
+        for (int i = 0; i < count; i++) {
+            persons[i] = new Person(scanner.nextLine());
+        }
+
+        final String[] configs = scanner.nextLine().split("\\s+");
+
+        final PersonSelectionAlgorithm alg = create(configs[0], Integer.parseInt(configs[1]));
+        SelectionContext ctx = new SelectionContext();
+        ctx.setAlgorithm(alg);
+
+        final Person[] selected = ctx.selectPersons(persons);
+        for (Person p : selected) {
+            System.out.println(p.name);
+        }
+    }
+
+    public static PersonSelectionAlgorithm create(String algType, int param) {
+        switch (algType) {
+            case "STEP": {
+                return new TakePersonsWithStepAlgorithm(param);
+            }
+            case "LAST": {
+                return new TakeLastPersonsAlgorithm(param);
+            }
+            default: {
+                throw new IllegalArgumentException("Unknown algorithm type " + algType);
+            }
+        }
+    }
+}
