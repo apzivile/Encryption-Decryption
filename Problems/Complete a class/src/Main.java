65274fb7ea4a8e98d85defc5b22e0818cb4e8e8f
apzivile <zivile.apo@gmail.com>
2020-07-06 13:29:17 +0100

    additional; Singleton problems

---
+++ Problems/Complete a class/src/Main.java
@@ -0,0 +1,13 @@
+class Singleton {
+    private Singleton() {
+    }
+
+    private static Singleton instance;
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
